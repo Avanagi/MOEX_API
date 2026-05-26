3554039f713a7ba1f03452b168370e89692ddbee
Glebass <glebnikolaev005@gmail.com>
2026-05-26 12:08:08 +0300

    feat: add CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import instruments
 
 app = FastAPI(
     title="MOEX Instruments API",
     description="API для поиска и фильтрации финансовых инструментов Московской биржи",
     version="1.0.0",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["GET"],
+    allow_headers=["*"],
 )
 
 app.include_router(instruments.router, prefix="/api")
